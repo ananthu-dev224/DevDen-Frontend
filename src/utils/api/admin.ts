3f73f07091e334fead7228880e4001d7d29d8c3f
Ananthu <ananthuks9526@example.com>
2024-07-13 11:49:02 +0530

    fix:axios

---
+++ src/utils/api/admin.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { baseUrl } from '../../constants/url';
+import { store } from '../../redux/store';
+import { adminLogout } from '../../redux/reducers/adminSlice';
+import { toast } from 'sonner';
+
+const adminInstance = axios.create({
+    baseURL: baseUrl,
+    withCredentials: true,
+})
+
+
+adminInstance.interceptors.request.use(
+    config => {
+        const state = store.getState();
+        const token = state.admin.token;
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+
+
+
+export default adminInstance;
