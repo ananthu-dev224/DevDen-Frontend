cf787600238462ddf7777669a3886fc0d2522fff
Ananthu <ananthuks9526@example.com>
2024-09-03 20:10:10 +0530

    feat: clear notifications and history

--- src/Pages/User/Notifications.tsx
+++ src/Pages/User/Notifications.tsx
@@ -1,9 +1,22 @@
-import { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect, CSSProperties } from "react";
 import { useDispatch } from "react-redux";
 import Navbar from "../../Components/Navbar";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
-import { getNotifications } from "../../services/notifications";
+import { getNotifications, clearNoti } from "../../services/notifications";
 import { formatTimestamp } from "../../utils/chatTime";
+import { confirmAlert } from "react-confirm-alert";
+import ScaleLoader from "react-spinners/ScaleLoader";
+import { toast } from "sonner";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "black",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+};
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -12,25 +25,59 @@ function classNames(...classes: string[]) {
 const Notifications: FC = () => {
   const [notifications, setNotifications] = useState<any[]>([]);
   const [history, setHistory] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchNoti = async () => {
-      const res = await getNotifications(dispatch);
-      if (res.status === "success") {
-        setNotifications(res.notifications);
-        setHistory(res.history);
-      }
-    };
+  const fetchNoti = async () => {
+    const res = await getNotifications(dispatch);
+    if (res.status === "success") {
+      setNotifications(res.notifications);
+      setHistory(res.history);
+    }
+  };
 
+  useEffect(() => {
     fetchNoti();
   }, []);
+
+  const handleClearNoti = async () => {
+    confirmAlert({
+      title: "Confirm Alert!",
+      message: "do you want to clear all notifications and history?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: async () => {
+            try {
+              setLoading(true);
+              const result = await clearNoti(dispatch);
+              setLoading(false);
+              if (result.status === "success") {
+                toast.success("Notifications cleared successfully");
+                fetchNoti();
+              }
+            } catch (error) {
+              console.error("Failed to clear notifications:", error);
+            }
+          },
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
+  };
 
   return (
     <div className="flex bg-gray-200 min-h-screen">
       <Navbar />
       <div className="flex-1 p-4 md:p-10 ml-0 md:ml-72 overflow-auto mb-6">
         <div className="bg-white shadow-md rounded-lg p-4">
+          <div className="flex justify-end">
+            <button className="text-gray-600" onClick={handleClearNoti}>
+              Clear all notifications and history?
+            </button>
+          </div>
           <TabGroup>
             <TabList className="flex border-b">
               <Tab
@@ -56,40 +103,69 @@ const Notifications: FC = () => {
             </TabList>
             <TabPanels className="mt-4">
               <TabPanel>
-                <div className="space-y-4">
-                {notifications.length > 0 ? (
+                <div
+                  className="space-y-4 overflow-y-auto scrollbar-hide"
+                  style={{ maxHeight: "calc(95vh - 200px)" }}
+                >
+                  {notifications.length > 0 ? (
                     notifications.map((noti) => (
-                      <div key={noti.id} className="p-4 bg-gray-100 rounded-lg flex justify-between">
-                        <p className="text-sm text-gray-700">
-                          {noti.text}
+                      <div
+                        key={noti.id}
+                        className="p-4 bg-gray-100 rounded-lg flex justify-between"
+                      >
+                        <p className="text-sm text-gray-700">{noti.text}</p>
+                        <p className="text-xs text-gray-500">
+                          {formatTimestamp(noti.createdAt)}
                         </p>
-                        <p className="text-xs text-gray-500">{formatTimestamp(noti.createdAt)}</p>
                       </div>
                     ))
                   ) : (
-                    <p className="text-center text-gray-500">No notifications available.</p>
+                    <p className="text-center text-gray-500">
+                      No notifications available.
+                    </p>
                   )}
                 </div>
               </TabPanel>
               <TabPanel>
-                <div className="space-y-4">
-                {history.length > 0 ? (
-                     <div>
-                     <p className="text-sm text-gray-500">Showing the latest history.</p>
-                     {history.map((value, index) => (
-                       <div key={index} className="p-4 bg-gray-100 rounded-lg mt-4">
-                         <p className="text-sm text-gray-700">{value}</p>
-                       </div>
-                     ))}
-                   </div>
+                <div
+                  className="space-y-4 overflow-y-auto scrollbar-hide"
+                  style={{ maxHeight: "calc(95vh - 200px)" }}
+                >
+                  {history.length > 0 ? (
+                    <div>
+                      <p className="text-sm text-gray-500">
+                        Showing the latest history.
+                      </p>
+                      {history.map((value, index) => (
+                        <div
+                          key={index}
+                          className="p-4 bg-gray-100 rounded-lg mt-4"
+                        >
+                          <p className="text-sm text-gray-700">{value}</p>
+                        </div>
+                      ))}
+                    </div>
                   ) : (
-                    <p className="text-center text-gray-500">No payment history available.</p>
+                    <p className="text-center text-gray-500">
+                      No payment history available.
+                    </p>
                   )}
                 </div>
               </TabPanel>
             </TabPanels>
           </TabGroup>
         </div>
+        {loading && (
+          <div className="fixed inset-0 bg-gray-100 bg-opacity-75 flex items-center justify-center z-50">
+            <ScaleLoader
+              color="black"
+              loading={loading}
+              cssOverride={override}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -25,3 +25,26 @@ export const getNotifications = async (dispatch:any): Promise<any> => {
       }
     }
 };
+
+// clear all notifications : /user/clear-notifications
+export const clearNoti = async (dispatch:any): Promise<any> => {
+  try {
+    const response = await api.get('/user/clear-notifications')
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      console.log(error.response)
+      const message = error.response.data.message || "An error occurred";
+      toast.error(message);
+
+      // Check for token verification and authorization errors
+      if (message === "Token expired" || message === 'No token in request' || message === "Failed to authenticate token" || message === "Your access has been restricted by the admin." || message === "Access Denied") {
+        dispatch(userLogout());
+      }
+
+      return error.response.data;
+    } else {
+      toast.error("An unexpected error occurred. Please try again later.");
+    }
+  }
+};
